a293d75e99cc80ce7654b0337bba079354930445
jgcaul <jgcaul@163.com>
2026-07-13 17:37:49 +0800

    Update scripts.data.js

--- docs/.vitepress/loaders/scripts.data.js
+++ docs/.vitepress/loaders/scripts.data.js
@@ -1,17 +1,18 @@
 import { createContentLoader } from 'vitepress'
+
+// 自定义项目展示顺序，前面加数字的文件名需要用引号
+const orderMap = {
+  'mengka': 1,
+  'bin23456789': 2,
+  '1keydd': 0,           
+  'leitbogioro': 3
+}
+
 export default createContentLoader('scripts/*.md', {
   watch: true,
   render: false,
   includeSrc: false,
   transform(raw) {
-    // 定义排序顺序
-    const orderMap = {
-      'mengka': 0,
-      'bin23456789': 1,
-      '1keydd': 2,
-      'leitbogioro': 3
-    }
-    
     // 提取文件名（不含扩展名）作为排序key
     const getFileKey = (url) => url.split('/').pop().replace('.md', '')
     
